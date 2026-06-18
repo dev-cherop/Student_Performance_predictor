32c8618adc44c2a7f6df7e3f08a161d4c8e1a260
dev-cherop <cybahuntaa@gmail.com>
2026-06-18 10:31:13 +0300

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,4 @@
-import logging
+from src.logger import logging
 import sys
 
 def error_message_detail(error, error_detail: sys):
@@ -22,5 +22,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
 
